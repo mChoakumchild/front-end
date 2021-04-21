ab5de27a4e42cd9cac270823b9a7eff712e4309b
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-21 17:28:09 -0400

    Stuff

--- index.js
+++ index.js
@@ -62,50 +62,68 @@
 
 let userBox = document.querySelector("ul#userlist")
 let recipelist = document.querySelector("ul#recipes")
-let ingredientUl = document.querySelector("ul#ingredients");
+let ingredientUl = document.querySelector("ul#ingredientsz");
 let stepsOl = document.querySelector("ol#steps");
-fetch("http://localhost:3000/users")
+
+// Unstable Login Form
+
+let userInput = document.querySelector("form#userlogin")// form
+let userName = document.querySelector("input.userid") // txtbox
+
+userInput.addEventListener('submit', (e)=> {
+  e.preventDefault()
+let username  = userName.value
+console.log(username)
+login(username)
+})
+
+// UNstable login function
+
+function login(username) { 
+
+fetch(`http://localhost:3000/users?_embed=recipes&username=${username}`)
   .then((r) => r.json())
-  .then((userArray) => {
-    //console.log(userArray);
+  .then((userObj) => {
+    if (userObj.length > 0) {
+      userObj = userObj[0]
+    showUserInformation(userObj)
+    }
+    else {
+      alert("No user exists")
+    }
+  });
+}
 
-    userArray.forEach((userObj) => { // userObj -> woody -> buzz
-      let username = userObj.username;
-      let userimage = document.createElement("img")
-      userimage.id = username
-      userimage.src = userObj.profilepic;
-      
+// USER INFO AND RECIPES
 
+function showUserInformation(userObj) {
+  
 
-      userimage.addEventListener("click", (event)=> { 
-        recipelist.innerText = ""  // reset Previous Recipes
+    let userimage = document.createElement("img")
+    userimage.src = userObj.profilepic;
+   
+    userBox.innerText = "" // Reset images
+    recipelist.innerText = ""  // reset Previous Recipe
 
-        userObj.recipes.forEach((recipeobj)=> {  
-        let recipeli = document.createElement('li')
-        recipeli.innerText = recipeobj.name
-        console.log(recipeobj)
+      userObj.recipes.forEach((recipeobj)=> {  
+      let recipeli = document.createElement('li')
+      recipeli.innerText = recipeobj.name
+      recipelist.append(recipeli)
         //EVENTLISTENR TO RECIPE IN RECIPE LIST
         recipeli.addEventListener("click", (event) => {
           newRecipe(recipeobj);
         })
-        recipelist.append(recipeli)
-
-          })
-      })
-      userBox.append(userimage)
-      // let recipeArray = userObj.recipes;
-      // // Recipe Display Function
-      // newRecipe(recipeArray[0]);
       
-
-
-    });
-  });
-
-function loadUsers(){
-
+      })
   
+    userBox.append(userimage)
+    // let recipeArray = userObj.recipes;
+    // // Recipe Display Function
+    // newRecipe(recipeArray[0]);
+    
+
 }
+
 
 recipeDiv = document.querySelector("div#displaydiv");
 //FUNCTION creates steps and ingredients LISTS
@@ -126,43 +144,39 @@ function newRecipe(recipe) {
   likeCount.innerText = `${recipe.likes} \u2665❤️✈️`; // cmd + ctr + space form imogi
 
   let ingredients = recipe.ingredients;
-  
+
 
   ingredients.forEach((item) => {
-    let newLi = document.createElement("li");
-    newLi.innerText = item;
-    ingredientUl.append(newLi);
+ 
+    newList(ingredientUl, item)
   });
 
   let steps = recipe.steps;
   
-
   steps.forEach((item) => {
-    let newLi = document.createElement("li");
-    newLi.innerText = item;
-    stepsOl.append(newLi);
+    newList(stepsOl,item)
   });
 }
 
-function loadIngredients() {}
-//function newList () {
-//   let newLi = document.createElement("li");
-//   newLi.innerText = stepDescription.value;
-//   //Delete Button
-//   let deleteB = document.createElement("button");
-//   deleteB.innerText = "Delete";
-//   deleteB.type = "delete";
-//   deleteB.class = "usrInput";
-//   // creating an edit button
-//   let editB = document.createElement("button");
-//   editB.innerText = "Edit";
-//   editB.type = "edit";
-//   editB.class = "usrInputEdit";
-//   // appending new elements to existing ones in the document.
-//   newLi.append(editB);
-//   newLi.append(deleteB);
-//   list.append(newLi);
-
+function newList(list, item) {
+  
+  let newLi = document.createElement("li");
+  newLi.innerText = item;
+  //Delete Button
+  let deleteB = document.createElement("button");
+  deleteB.innerText = "Delete";
+  deleteB.type = "delete";
+  deleteB.class = "usrInput";
+  // creating an edit button
+  let editB = document.createElement("button");
+  editB.innerText = "Edit";
+  editB.type = "edit";
+  editB.class = "usrInputEdit";
+  // appending new elements to existing ones in the document.
+  newLi.append(editB);
+  newLi.append(deleteB);
+  list.append(newLi);
+}
 //   // EVENTLISTENER for DELETE AND EDIT
 
 //   editB.addEventListener("click", (e) => {
